7491d64770826a777a64312e92065693baa831d0
Devansh <devansh.madd@gmail.com>
2021-06-22 17:46:16 +0400

    with services update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Cybersecurity from "./components/Cybersecurity";
+import IOT from "./components/IOT";
+import Robotics from "./components/Robotics";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Landing /> 
-      {/* <Cybersecurity />
+      <Cybersecurity />
       <IOT /> 
-      <Robotics /> */}
+      <Robotics />
     </div>
   );
 }
