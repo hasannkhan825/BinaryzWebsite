46e0ba4b59695fee3c1e83085d39813152f58a54
Devansh <devansh.madd@gmail.com>
2021-06-22 17:54:04 +0400

    final changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing";
 import Cybersecurity from './components/Cybersecurity';
+import IOT from './components/IOT';
+import Robotics from './components/Robotics';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -30,7 +30,6 @@ class Navbar extends React.Component {
                         )
                     })}
                 </ul>
-                <Button>Sign up</Button>
             </nav>
         )
     }
