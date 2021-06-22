7fed80f17d96a02654a8b11bacf4e72374112900
Devansh <devansh.madd@gmail.com>
2021-06-22 16:14:08 +0400

    components for services

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing";
+import Cybersecurity from './components/Cybersecurity';
+import IOT from './components/IOT';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Landing /> 
-      {/* <Cybersecurity />
+       <Cybersecurity />
       <IOT /> 
-      <Robotics /> */}
+      {/*<Robotics /> */}
     </div>
   );
 }
