6a0b1fa9222c6dcc4ddbc3e9746380a034c95ec9
Hasan Vasim Khan <f20170102@dubai.bits-pilani.ac.in>
2021-06-22 11:58:34 +0300

    sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Landing /> 
+      {/* <Cybersecurity />
+      <IOT /> 
+      <Robotics /> */}
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Landing.css';
+
+const Landing = () => {
+
+
+    return(
+        <div className='Heading'>The binaryz</div>
+    )
+
+
+}
+
+
+export default Landing;
